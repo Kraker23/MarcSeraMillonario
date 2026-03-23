9ab59f86129ac242528d7031dba5f24c66752087
Cristian Kraker <thekraker23@gmail.com>
2026-03-23 18:25:11 +0100

    Funciona Base

---
+++ Concurso/Concurso.Web/Shared/Questionario.cs
@@ -0,0 +1,7 @@
+﻿namespace Concurso.Web.Shared
+{
+    public class Questionario
+    {
+        public  List<Pregunta> preguntas { get; set; }
+    }
+}
---
+++ Concurso/Concurso.Web/Shared/Respuesta.cs
@@ -0,0 +1,9 @@
+﻿namespace Concurso.Web.Shared
+{
+    public class Respuesta
+    {
+        public Guid IdRespuesta { get; set; }
+        public string textoRespuesta { get; set; }
+        public bool esCorrecta { get; set; }
+    }
+}
